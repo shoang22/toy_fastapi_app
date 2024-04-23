2ff502fbb8d9b180db10985d065f648d5f86d72b
shoang22 <54875725+shoang22@users.noreply.github.com>
2024-04-23 17:15:36 -0400

    add profiler

--- src/app.py
+++ src/app.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from contextlib import asynccontextmanager
+from fastapi_profiler import PyInstrumentProfilerMiddleware
 
 from src.routes import router
 from src.broker import broker
@@ -24,5 +25,15 @@ async def lifespan(app: FastAPI):
 
 def get_app() -> FastAPI:
     app = FastAPI(lifespan=lifespan)
+    app.add_middleware(
+        PyInstrumentProfilerMiddleware,
+        server_app=app,  # Required to output the profile on server shutdown
+        profiler_output_type="html",
+        is_print_each_request=True,  # Set to True to show request profile on
+        # stdout on each request
+        open_in_browser=False,  # Set to true to open your web-browser automatically
+        # when the server shuts down
+        html_file_name="example_profile.html",  # Filename for output
+    )
     app.include_router(router)
     return app
